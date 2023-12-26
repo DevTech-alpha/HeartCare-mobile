acfc1f8f60e90356c216fdba5afe94a23809f811
Juliano Santos <128374665+DevSntosx71@users.noreply.github.com>
2023-12-26 19:03:30 -0300

    new questions

--- src/screens/FAQ/Index.tsx
+++ src/screens/FAQ/Index.tsx
@@ -1,22 +1,62 @@
-// FAQScreen.js
+
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { styles } from './styles';
 import * as Animatable from 'react-native-animatable';
 
-const FAQScreen = () => {
+function FAQScreen() {
   const [questions] = useState([
-    { id: 1, question: 'O que é um transplante cardíaco?', answer: 'Um transplante cardíaco é a substituição de um coração doente por um coração saudável de um doador. Esse procedimento é realizado para tratar condições graves do coração, como insuficiência cardíaca avançada ou danos irreparáveis.' },
-    { id: 2, question: 'Quem pode ser um candidato para um transplante cardíaco?', answer: 'Os candidatos para um transplante cardíaco são geralmente pacientes com insuficiência cardíaca grave que não respondem adequadamente a outras formas de tratamento. Avaliações médicas detalhadas são realizadas para determinar a elegibilidade.' },
-    { id: 3, question: 'Quais são os riscos associados ao transplante cardíaco?', answer: 'O transplante cardíaco envolve alguns riscos, como rejeição do órgão transplantado, infecções, complicações cirúrgicas, e efeitos colaterais dos medicamentos imunossupressores. A equipe médica monitora de perto o paciente para mitigar esses riscos.' },
-    { id: 4, question: 'Qual é a expectativa de vida após um transplante cardíaco?', answer: 'A expectativa de vida após um transplante cardíaco pode variar de paciente para paciente. Em geral, muitos pacientes conseguem levar vidas saudáveis por muitos anos. O acompanhamento médico contínuo e o cumprimento das orientações médicas são essenciais.' },
-    { id: 5, question: 'Como é o processo de recuperação pós-transplante cardíaco?', answer: 'O processo de recuperação pós-transplante cardíaco envolve cuidados intensivos no hospital seguidos por acompanhamento ambulatorial. Os pacientes passam por reabilitação cardíaca, ajustes de medicamentos e monitoramento constante para garantir uma recuperação adequada.' },
-    { id: 6, question: 'Existe algum cuidado especial necessário após o transplante?', answer: 'Sim, alguns cuidados especiais após o transplante cardíaco incluem a adesão rigorosa à medicação imunossupressora, acompanhamento médico regular, manutenção de um estilo de vida saudável, evitando fatores de risco e participação em programas de reabilitação cardíaca.' },
-    { id: 7, question: 'Quais são as alternativas ao transplante cardíaco?', answer: 'Em alguns casos, alternativas ao transplante cardíaco podem incluir tratamentos médicos avançados, dispositivos de assistência ventricular, ou outras intervenções cirúrgicas. A escolha da alternativa depende da condição específica de cada paciente.' },
-    { id: 8, question: 'Quais são os sintomas de rejeição após o transplante?', answer: 'Os sintomas de rejeição após o transplante cardíaco podem incluir fadiga, falta de ar, ganho de peso repentino, diminuição da função cardíaca e inchaço. É crucial relatar quaisquer sintomas à equipe médica imediatamente para avaliação e intervenção.' },
-    { id: 9, question: 'É possível realizar atividades físicas após o transplante cardíaco?', answer: 'Sim, é possível realizar atividades físicas após o transplante cardíaco. No entanto, é necessário seguir orientações médicas específicas, participar de programas de reabilitação cardíaca e adaptar o nível de atividade de acordo com a condição de saúde individual.' },
-    { id: 10, question: 'Quais são as complicações mais comuns pós-transplante cardíaco?', answer: 'Algumas das complicações mais comuns pós-transplante cardíaco incluem rejeição, infecções, problemas renais, hipertensão arterial, diabetes e efeitos colaterais dos medicamentos imunossupressores. A gestão adequada dessas complicações é fundamental para o bem-estar do paciente.' },
+    {
+      id: 1,
+      pergunta: 'O que é saúde cardiovascular?',
+      resposta: 'A saúde cardiovascular refere-se ao bem-estar do coração e dos vasos sanguíneos, envolvendo práticas que promovem a prevenção de doenças cardíacas.'
+    },
+    {
+      id: 2,
+      pergunta: 'Quais são os hábitos alimentares saudáveis para o coração?',
+      resposta: 'Uma dieta equilibrada, rica em frutas, vegetais, grãos integrais e com baixo teor de gorduras saturadas, ajuda a manter a saúde do coração e a reduzir o risco de doenças cardíacas.'
+    },
+    {
+      id: 3,
+      pergunta: 'Como a atividade física contribui para a saúde cardíaca?',
+      resposta: 'A prática regular de exercícios fortalece o coração, melhora a circulação sanguínea, controla a pressão arterial e reduz os níveis de colesterol, promovendo a saúde cardiovascular.'
+    },
+    {
+      id: 4,
+      pergunta: 'Quais são os sinais de alerta de problemas cardíacos?',
+      resposta: 'Sintomas como dor no peito, falta de ar, palpitações e fadiga inexplicável podem indicar problemas cardíacos. Consultar um profissional de saúde é fundamental diante desses sinais.'
+    },
+    {
+      id: 5,
+      pergunta: 'Qual é a importância do controle do estresse para o coração?',
+      resposta: 'O estresse crônico pode impactar negativamente o coração. Estratégias de gerenciamento do estresse, como meditação e atividades relaxantes, são benéficas para a saúde cardiovascular.'
+    },
+    {
+      id: 6,
+      pergunta: 'Como a qualidade do sono afeta o coração?',
+      resposta: 'Um sono adequado é vital para a recuperação do coração. A falta de sono pode contribuir para o desenvolvimento de condições cardíacas, sendo essencial manter uma rotina de sono saudável.'
+    },
+    {
+      id: 7,
+      pergunta: 'Quais são os benefícios da manutenção de um peso saudável?',
+      resposta: 'Manter um peso adequado reduz a carga sobre o coração, diminui o risco de doenças cardíacas e contribui para a saúde geral do sistema cardiovascular.'
+    },
+    {
+      id: 8,
+      pergunta: 'Como a cessação do tabagismo impacta a saúde do coração?',
+      resposta: 'Parar de fumar beneficia diretamente a saúde do coração, reduzindo o risco de doenças cardiovasculares. Os benefícios começam a ser notados logo após a cessação do hábito.'
+    },
+    {
+      id: 9,
+      pergunta: 'Quais são os exames de rotina essenciais para monitorar a saúde cardíaca?',
+      resposta: 'Exames como o check-up anual, a medição da pressão arterial, a análise do colesterol e exames específicos, quando indicados, são fundamentais para monitorar a saúde do coração.'
+    },
+    {
+      id: 10,
+      pergunta: 'Como a genética influencia a saúde cardiovascular?',
+      resposta: 'A predisposição genética pode influenciar o risco de doenças cardíacas. Compreender a história familiar e realizar avaliações médicas regulares são passos importantes para um cuidado preventivo.'
+    }
   ]);
 
 
@@ -44,18 +84,17 @@ const FAQScreen = () => {
               onPress={() => handleQuestionPress(item.id)}
             >
               <View style={styles.questionHeader}>
-              <Icon
+                <Icon
                   name={selectedQuestion === item.id ? 'chevron-up' : 'chevron-down'}
                   size={20}
-                  color="#333333"
-                />
-                <Text style={styles.questionText}>{item.question}</Text>
-               
+                  color="#333333" />
+                <Text style={styles.questionText}>{item.pergunta}</Text>
+
               </View>
             </TouchableOpacity>
             {selectedQuestion === item.id && (
               <Animatable.Text animation="fadeIn" style={styles.answerText}>
-                {item.answer}
+                {item.resposta}
               </Animatable.Text>
             )}
           </Animatable.View>
@@ -63,7 +102,7 @@ const FAQScreen = () => {
       </ScrollView>
     </View>
   );
-};
+}
 
 export default FAQScreen;
 
